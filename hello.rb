de78f399ba08a719e57d716b047f9c4f31319212
fasse59 <109723429+fasse59@users.noreply.github.com>
2023-11-28 11:59:14 +0500

    Add files via upload

---
+++ hello.rb
@@ -0,0 +1,23 @@
+first=0
+second=1
+nextterm=0
+puts"enter the number of terms"
+n=gets.chomp.to_i
+puts"the first #{n} terms of fibonnaci series are"
+c=1
+while(c<=n+1)
+if c<=1
+nextterm=c
+else 
+puts nextterm
+nextterm=first+second
+first=second
+second= nextterm
+end
+c+=1
+end
+
+
+
+
+
